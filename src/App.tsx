a2cd1f5fc09e799e3a4656dcf54577d71d61d334
Jean Carlo <jean.carlo.latoure@gmail.com>
2024-03-08 12:07:41 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Profile from "./components/Profile"
-import MyForm from "./components/Contact"
+import MyForm from "./components/Form"
+import myImage from "./components/img/JC-Profile-Photo-2.jpg"
 
 const App = () => {
   return (
     <div>
-      <Profile/>
+      <Profile ProfilePic={myImage} ProfileIntro="I am a Front-End Developer with 15 years of experience working as a webmaster with my last employer.<br></br>My experience is developing user-friendly responsive websites using HTML, CSS, PHP and JavaScript. Creating mock-ups, layouts, graphics and banners, building a CMS database, using SEO optimization, Google Ads and Google Analytics."/>
       <MyForm/>
     </div>
   )
